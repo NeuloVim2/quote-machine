85d71f11db148a7c94d6b4f752d1392cb526a77e
46398295 <strelcov.stanislav93@gmail.com>
2020-11-14 15:15:46 +0200

    Updates

---
+++ precache-manifest.ba7b5497e227a28ebb9c3d9d975d757a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63db907312fae9595e5bd93f7e80a21c",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "fbe273fea50947da99da",
+    "url": "/quote-machine/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3d2be584d63038fcd725",
+    "url": "/quote-machine/static/css/main.e2e31b76.chunk.css"
+  },
+  {
+    "revision": "fbe273fea50947da99da",
+    "url": "/quote-machine/static/js/2.88ef626d.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/quote-machine/static/js/2.88ef626d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2be584d63038fcd725",
+    "url": "/quote-machine/static/js/main.774880f0.chunk.js"
+  },
+  {
+    "revision": "256db52c5a0c9de87c09",
+    "url": "/quote-machine/static/js/runtime-main.6ade76f3.js"
+  }
+]);
